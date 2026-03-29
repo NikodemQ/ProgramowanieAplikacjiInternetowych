0c431e36976e0815189bb5af80852fbeaf38bfd0
Nikodem Szlauer <nikodem@qware.pl>
2026-03-29 18:57:06 +0200

    Added swagger gen.

--- WeatherTelemetryApi/WeatherTelemetryApi/Program.cs
+++ WeatherTelemetryApi/WeatherTelemetryApi/Program.cs
@@ -1,8 +1,32 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Weather Telemetry API",
+        Version = "v1",
+        Description = "API do publikowania odczytów pogodowych na RabbitMQ."
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather Telemetry API v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
